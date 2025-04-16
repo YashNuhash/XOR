ebed3f0334f0d19823e8ab94cf2d03ab25f28fc1
NUHASHROXME <nuhashroxme@gmail.com>
2025-04-16 12:27:40 +0600

    Update not-found.tsx with recent changes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,4 @@
-import ErrorSection7 from "../components/error-section7";
+import {ErrorSection7} from "../components/error-section7";
 
 export default function NotFound() {
   return <ErrorSection7 />;
